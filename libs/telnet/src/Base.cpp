0173baa85734cdd3925b9880f14ea4f01ea8643e
Andrew Bastien <volundmush@gmail.com>
2026-01-27 11:18:14 -0500

    Added missing implementations.

--- libs/telnet/src/Base.cpp
+++ libs/telnet/src/Base.cpp
@@ -3,4 +3,29 @@
 namespace volcano::telnet {
 
 	TelnetLimits telnet_limits;
+
+	TelnetMessageSubnegotiation TelnetMessageGMCP::toSubnegotiation() const {
+		TelnetMessageSubnegotiation out;
+		out.option = codes::GMCP;
+		out.data = package;
+		if (!data.is_null()) {
+			out.data += " ";
+			out.data += data.dump();
+		}
+		return out;
+	}
+
+	TelnetMessageSubnegotiation TelnetMessageMSSP::toSubnegotiation() const {
+		TelnetMessageSubnegotiation out;
+		out.option = codes::MSSP;
+		std::string payload;
+		for (const auto& [key, value] : variables) {
+			payload.push_back(static_cast<char>(1));
+			payload += key;
+			payload.push_back(static_cast<char>(2));
+			payload += value;
+		}
+		out.data = std::move(payload);
+		return out;
+	}
 }
